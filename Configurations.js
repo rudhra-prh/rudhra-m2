f08ec6070cb6b4ae47f087080a77bc20eef55127
rudhra-prh <89623675+rudhra-prh@users.noreply.github.com>
2023-09-10 02:31:39 +0530

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -7,12 +7,12 @@ if (!gg) {
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
+global.mongodb = process.env.MONGODB || "mongodb+srv://rudhra:rudhraprh3@cluster0.wpoxqbg.mongodb.net/?retryWrites=true&w=majority";
 global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || ",";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Rudhra Bot`;
-global.author = process.env.AUTHOR || "Rudhra";
+global.packname = process.env.PACKNAME || `Ʀ ᴜ ᴅ ʜ ʀ λ`;
+global.author = process.env.AUTHOR || "Ƥ ʀ ɪ ɴ ᴄ ᴇ  Ʀ ᴜ ᴅ ʜ";
 global.port = process.env.PORT || "10000";
 global.openAiAPI = process.env.OPENAI_API || "Put your openai API key here";
 global.owner = gg.split(",");
